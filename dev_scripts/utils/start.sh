107b6eb90b80adc5ede098697880cc7bd621e81b
ouss1919 <oussama@myem.fr>
2022-11-03 16:16:18 +0100

    MYEM-3294 #time 30m #comment coorections in Jnekinsfile

--- dev_scripts/utils/start.sh
+++ dev_scripts/utils/start.sh
@@ -5,7 +5,7 @@ export INTEGRATION_TESTS_CONTAINERS_PREFIX=${2:-local}
 docker-compose -p $INTEGRATION_TESTS_CONTAINERS_PREFIX up -d
 sleep 10
 
-API_URL=http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${PREFIX}_api_1):3000
+API_URL=http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${INTEGRATION_TESTS_CONTAINERS_PREFIX}_api_1):3000
 export API_URL=$API_URL
 API_KEY=$(python3 get_api_key.py)
 
