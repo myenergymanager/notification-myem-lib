b3570a4675a5e0d992bc51ec64e8bb007b058619
youcef <youcef@myem.fr>
2023-01-24 16:35:25 +0100

    MYEM-3835 #time 10m #comment fixe tests

--- tests/novu_manager/events_manager/test_unit.py
+++ tests/novu_manager/events_manager/test_unit.py
@@ -5,24 +5,6 @@ from notification_lib.http_requests import HttpRequester
 
 class TestEventsManager:
     def test_trigger_event(self, novu, created_notification_template, created_subscriber):
-
-        # since we've made that the preferences of the templates will be set to False
-        # the subscriber should not receive any targeted event.
-        for _ in range(5):
-            novu.events_manager.trigger_event(
-                template_name="template name",
-                recipients=created_subscriber["subscriber_id"],
-                payload={},
-            )
-        messages = HttpRequester.send_request(
-            operation="GET",
-            endpoint=f"/v1/messages?channel=in_app&"
-            f"subscriberId={created_subscriber['subscriber_id']}",
-        ).json()
-
-        assert len(messages["data"]) == 0
-
-        # now we change the subscriber's preferences to True
         template = novu.notification_templates_manager.get_template_by_name(
             template_name="template name"
         )
